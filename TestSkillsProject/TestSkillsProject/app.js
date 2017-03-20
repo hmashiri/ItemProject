42423289551460ab89b211143be357378a02af05
Hadjir Mashiri <hadjirmashiri@gmail.com>
2017-03-20 00:07:40 -0700

    final work completed

--- TestSkillsProject/TestSkillsProject/app.js
+++ TestSkillsProject/TestSkillsProject/app.js
@@ -8,12 +8,45 @@
 
     function ItemController($http) {
         var vm = this;
-
+        vm.showHighestPrice = false;
+        vm.showAllItems = true;
+        
+     
         console.log('getting inside controller');
 
         $http.get('http://localhost:56307/api/item').then(function (response) {
             vm.data = response.data;
-            console.log(vm.data);
+       
         })
+
+        $http.get('http://localhost:56307/api/item/highest').then(function (response) {
+            vm.highestPriceData = response.data;
+      
+        })
+
+
+        vm.itemByLowestPrice = function ()
+        {
+            var url = 'http://localhost:56307/api/item?ItemName=' + vm.lowestItem;
+
+            console.log('about to call url : ' + url + ' to get the lowest price'); 
+            $http.get(url).then(function (response) {
+                vm.itemByLowest = response.data;
+
+            })
+        }
+        
+
+        vm.showHighest = function() {
+            vm.showHighestPrice = true;
+            vm.showAllItems = false;
+        }
+
+        vm.showAll = function () {
+            vm.showHighestPrice = false;
+            vm.showAllItems = true;
+        }
     }
+
+        
 })();
